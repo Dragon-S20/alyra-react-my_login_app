af8bb1babbd7ee425e6ac9b68f9c99248f16fbd9
mihaidld <mihaidoldur@gmail.com>
2020-10-05 10:35:20 +0200

    react app with form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LoginForm from "./LoginForm.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-4">
+      <h1 className="display-3 text-center mb-4">Authentification</h1>
+      <LoginForm />
     </div>
   );
 }
